7403fda46d85560915940242c40c83b5c246887a
zakieki55 <zakueki@gmail.com>
2022-10-31 20:02:53 +0700

    make a new project

---
+++ src/main/java/ws/b/takehome1/Takehome1Application.java
@@ -0,0 +1,13 @@
+package ws.b.takehome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Takehome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Takehome1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/b/takehome1/Takehome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.b.takehome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takehome1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
